252ab3da1154bcaed6236bcb5d4235591886a23b
Kushagra Sahani <96765538+kushagrasahani@users.noreply.github.com>
2025-10-24 00:27:56 +0530

    Two pointers easy to med level questions

---
+++ dsarevision24oct.cpp
@@ -0,0 +1,40 @@
+//  Pattern - 2 pointers
+//  used when we need to find a pair that satifies a target or whenever we need to remove duplicates as a pair
+
+//  Q1 - Remove Duplicates from Sorted Array
+ int removeDuplicates(vector<int>& nums) {
+        int k = 0;
+        int i = 1;
+        while (i < nums.size()) {
+            if (nums[i] == nums[k]) {
+                i++;
+            } else {
+                k++;
+                nums[k] = nums[i];
+                i++;
+            }
+        }
+        return k + 1;
+    }
+
+    // Q2 - Remove Element - imp to understand pattern
+     int removeElement(vector<int>& nums, int val) {
+        if (nums.size() == 1) {
+            if (nums[0] == val)
+                return 0;
+            return 1;
+        }
+        int k = 0;
+        int i = 0;
+        while (i<nums.size()) {
+           if(nums[i]!= val){
+            nums[k] = nums[i];
+            i++;
+            k++;
+           }
+            else {
+                i++;
+            }
+        }
+        return k;
+    }
