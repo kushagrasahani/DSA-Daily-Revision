a74ec053309e8404768e57d151ffaa742b6d08a2
Kushagra Sahani <96765538+kushagrasahani@users.noreply.github.com>
2025-10-24 23:48:05 +0530

    Update dsarevision24oct.cpp
    practiced more on 2 pointer

--- dsarevision24oct.cpp
+++ dsarevision24oct.cpp
@@ -37,4 +37,27 @@
             }
         }
         return k;
+
+    }
+
+
+
+  // Q3 - container with most water
+  // bs dekh area = w * h , ab width to dec kr hi rhi hai , to l inc krni padegi
+  // to isiliye jb bhi choti length ho to uss direction ka pointer inc kro
+
+     int maxArea(vector<int>& height) {
+        int maxi = 0;
+        int i=0;
+        int j = height.size()-1;
+        while(i < j){
+            int h = min(height[i], height[j]);
+            int w = j-i;
+            maxi = max(maxi , h*w);
+            if(height[i] < height[j]){
+                i++;
+            }
+            else j--;
+        }
+        return maxi;
     }
